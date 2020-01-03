7a418074089d860f58552ae62bc03c8c7181370f
unknown <ramazan_firin@hotmail.com>
2020-01-03 09:52:46 +0300

    deletefiles geçici olarak iptal edildi

--- src/main/java/com/mastertek/service/FileCatalogService.java
+++ src/main/java/com/mastertek/service/FileCatalogService.java
@@ -38,7 +38,7 @@ public class FileCatalogService {
 	
 	}
     
-    @Scheduled(cron = "0 0 4 * * ?") //This is scheduled to get fired everyday, at 01:00 (am).
+    //@Scheduled(cron = "0 0 4 * * ?") //This is scheduled to get fired everyday, at 01:00 (am).
     //@Transactional(propagation = Propagation.REQUIRES_NEW)
     public void deleteFiles() throws InterruptedException {
     	
